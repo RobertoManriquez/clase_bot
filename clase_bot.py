c22eca98d5c3e0e61aedbed98593e530b0f2b51e
RobertoManriquez <roberto.manriquez2009@gmail.com>
2025-01-14 20:06:46 -0300

    Add files via upload

---
+++ clase_bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("MTMyNjMzMjgyNjk5NTY1ODc3Mg.G1FP7s.m69Mu02XDRntv10wi_y7BG3yP0-5MOZJU5h9M0")
