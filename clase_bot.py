fcdae8e4fe26d4048092147bc8f8efb3a74e346d
RobertoManriquez <roberto.manriquez2009@gmail.com>
2025-01-28 13:28:28 -0300

    clase_bot.py
    Añadí comandos de memes, entre otros.

--- clase_bot.py
+++ clase_bot.py
@@ -1,6 +1,8 @@
 import discord
 import random
+import os
 import asyncio
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -59,5 +61,25 @@ async def juego(ctx, valorgame: int):
                 
                 score += sumascore
     await ctx.send(f"¡Acabó el juego! Tu puntuación final es: {score}")
+
+@bot.command()
+async def meme(ctx):
+    img_meme = random.choice(os.listdir('images'))
+    with open(f'images/{img_meme}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file = picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 bot.run(token)
